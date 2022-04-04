76178a69b1551cee79fd087e2318a33a808b1b97
porludom <porludom@mail.ru>
2022-04-04 13:39:34 +0300

    .

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,50 @@
 // your solution here
 //fix all the errors including package errors
+package ru.innopolis.problem2;
+class Main{
+    public static void main(String[] args) {
+        ChargeStation station = new ChargeStation();
+        Android mobile = new Android();
+        station.chargeLightning(new UsbToLightningAdapter(mobile));
+    }
+}
+class ChargeStation{
+    public void chargeLightning(Lightning device){
+
+    }
+}
+
+interface USB{
+    void chargeUSB();
+}
+
+interface Lightning{
+    void chargeLightning();
+}
+
+class UsbToLightningAdapter implements Lightning{
+    private USB USBDevice;
+    @Override
+    public void chargeLightning() {
+        USBDevice.chargeUSB();
+    }
+
+    public UsbToLightningAdapter(USB USBDevice) {
+        this.USBDevice = USBDevice;
+    }
+}
+
+class Android implements USB{
+
+    @Override
+    public void chargeUSB() {
+
+    }
+}
+class Iphone implements Lightning{
+
+    @Override
+    public void chargeLightning() {
+
+    }
+}
