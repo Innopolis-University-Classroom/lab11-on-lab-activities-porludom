c66677be10a70b56416060c89d9deac98626b622
porludom <porludom@mail.ru>
2022-04-04 13:13:20 +0300

    .

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,30 @@
 // your solution here
 //fix all the errors including package errors
+package ru.innopolis.problem1;
+
+class Workshop{
+    private Operation operation;
+    public void operate(){
+        operation.operate();
+    }
+    public void setOperation(Operation op){
+        operation = op;
+    }
+}
+interface Operation{
+    void operate();
+}
+class AssembleCar implements Operation{
+
+    @Override
+    public void operate() {
+
+    }
+}
+class DisassempleCar implements Operation{
+
+    @Override
+    public void operate() {
+
+    }
+}
